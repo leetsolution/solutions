2ca5fb15c17bb36b615cb07413f1950fdd99770f
msaswata15 <121812665+msaswata15@users.noreply.github.com>
2025-08-07 19:48:11 +0530

    Update timestamps in problem.json files for rides I and II

--- problems/3633-earliest-finish-time-for-land-and-water-rides-i/solution.py
+++ problems/3633-earliest-finish-time-for-land-and-water-rides-i/solution.py
@@ -1,19 +1,9 @@
 class Solution:
-    def earliestFinishTime(landStartTime, landDuration, waterStartTime, waterDuration):
-        min_finish_time = float('inf')
-
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        ans = float('inf')
         for i in range(len(landStartTime)):
             for j in range(len(waterStartTime)):
-                # Land then Water
                 land_finish = landStartTime[i] + landDuration[i]
-                water_start = max(land_finish, waterStartTime[j])
-                water_finish = water_start + waterDuration[j]
-                min_finish_time = min(min_finish_time, water_finish)
-
-                # Water then Land
                 water_finish = waterStartTime[j] + waterDuration[j]
-                land_start = max(water_finish, landStartTime[i])
-                land_finish = land_start + landDuration[i]
-                min_finish_time = min(min_finish_time, land_finish)
-
-        return min_finish_time
+                ans = min(ans, max(land_finish, waterStartTime[j]) + waterDuration[j], max(water_finish, landStartTime[i]) + landDuration[i])
+        return ans
