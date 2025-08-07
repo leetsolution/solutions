50e43c9989d27d8eb67eeeb5d2c3025222faf688
leetsolution <msaswata15gdsc@gmail.com>
2025-08-07 09:42:23 +0530

    Update solution.java

--- problems/3592-inverse-coin-change/solution.java
+++ problems/3592-inverse-coin-change/solution.java
@@ -1,7 +1,28 @@
 import java.util.*;
+
 class Solution {
-    public List<Integer> findCoins(int[] numWays) {
-        // TODO: Implement the logic for inverse coin change
-        return new ArrayList<>();
+    public static List<Integer> findCoins(int[] numWays) {
+        int n = numWays.length;
+        int[] dp = new int[n + 1];
+        List<Integer> coins = new ArrayList<>();
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            int amount = i + 1;
+            int expectedWays = numWays[i];
+
+            if (expectedWays > 0 && dp[amount] == expectedWays - 1) {
+                coins.add(amount);
+                for (int j = amount; j <= n; j++) {
+                    dp[j] += dp[j - amount];
+                }
+            }
+
+            if (dp[amount] != expectedWays) {
+                return new ArrayList<>();
+            }
+        }
+
+        return coins;
     }
 }
