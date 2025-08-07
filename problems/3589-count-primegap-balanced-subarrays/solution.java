4bd384b9a6f457099b6598b14017b08b35219b6f
leetsolution <msaswata15gdsc@gmail.com>
2025-08-07 09:34:24 +0530

    Update solution.java

--- problems/3589-count-primegap-balanced-subarrays/solution.java
+++ problems/3589-count-primegap-balanced-subarrays/solution.java
@@ -1,27 +1,78 @@
+import java.util.*;
+
 class Solution {
-    public int primeSubarray(int[] nums, int k) {
-        int n = nums.length;
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            int min = nums[i], max = nums[i];
-            for (int j = i; j < n; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-                if (isPrime(max - min) && (max - min) >= k) {
-                    count++;
+    // Generate prime numbers up to 50000 using Sieve of Eratosthenes
+    public int[] primeNumbers() {
+        int MAX_NUM = 10000;
+        int[] prime = new int[5 * MAX_NUM + 1];
+        int n = prime.length;
+
+        Arrays.fill(prime, 1);
+        prime[0] = prime[1] = 0;
+
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i] == 1) {
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = 0;
                 }
             }
         }
-        return count;
+        return prime;
     }
 
-    private boolean isPrime(int x) {
-        if (x < 2) return false;
-        if (x == 2) return true;
-        if (x % 2 == 0) return false;
-        for (int i = 3; i * i <= x; i += 2) {
-            if (x % i == 0) return false;
+    // Maintain min and max deques for current window
+    public void insertPrimeDQ(Deque<Integer> minDQ, Deque<Integer> maxDQ, int[] nums, int index) {
+        while (!minDQ.isEmpty() && nums[minDQ.peekLast()] > nums[index]) {
+            minDQ.removeLast();
         }
-        return true;
+        minDQ.addLast(index);
+
+        while (!maxDQ.isEmpty() && nums[maxDQ.peekLast()] < nums[index]) {
+            maxDQ.removeLast();
+        }
+        maxDQ.addLast(index);
+    }
+
+    public int primeSubarray(int[] nums, int k) {
+        int[] prime = primeNumbers(); // get all primes up to 50000
+
+        Deque<Integer> minDQ = new ArrayDeque<>();
+        Deque<Integer> maxDQ = new ArrayDeque<>();
+        Deque<Integer> primeDQ = new ArrayDeque<>();
+
+        int left = 0;
+        int prevPrime = 0;
+        int result = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int val = nums[i];
+
+            if (val < prime.length && prime[val] == 1) {
+                insertPrimeDQ(minDQ, maxDQ, nums, i);
+
+                if (!primeDQ.isEmpty()) {
+                    prevPrime = primeDQ.peekLast();
+                }
+                primeDQ.addLast(i);
+
+                // Shrink window if prime gap exceeds k
+                while (!minDQ.isEmpty() && !maxDQ.isEmpty() &&
+                       nums[maxDQ.peekFirst()] - nums[minDQ.peekFirst()] > k) {
+
+                    int removeIdx = primeDQ.removeFirst();
+                    left = removeIdx + 1;
+
+                    if (!minDQ.isEmpty() && minDQ.peekFirst() <= removeIdx) minDQ.removeFirst();
+                    if (!maxDQ.isEmpty() && maxDQ.peekFirst() <= removeIdx) maxDQ.removeFirst();
+                }
+            }
+
+            if (primeDQ.size() >= 2) {
+                result += prevPrime - left + 1;
+            }
+        }
+
+        return result;
     }
 }
